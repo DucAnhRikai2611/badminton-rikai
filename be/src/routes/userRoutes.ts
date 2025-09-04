a7eb04fb087da4fb0e5ff086a534236c5e4e0977
ADMIN\Admin <ducanhmc24@gmail.com>
2025-09-04 20:23:14 +0700

    (Update) Reorganize

--- be/src/routes/userRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router, Request, Response } from "express";
-const router = Router();
-
-router.get("/", (req: Request, res: Response) => {
-  res.json({ message: "User route works!" });
-});
-
-export const userRoutes = router;
