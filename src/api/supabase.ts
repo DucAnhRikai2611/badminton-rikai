b93794b5653c788764e4af54387dc50d57dfed9b
ADMIN\Admin <ducanhmc24@gmail.com>
2025-09-04 23:55:21 +0700

    (Add) API base config

---
+++ src/api/supabase.ts
@@ -0,0 +1,4 @@
+import { supabaseAnonKey, supabaseUrl } from "@/configs";
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
